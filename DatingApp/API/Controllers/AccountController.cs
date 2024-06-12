d85f0ca2bc30c8d385118117c9c219698ad86b16
Saugata Rana.rana <saugata_rana@yahoo.com>
2024-06-12 18:22:06 +0530

    Initial commit

---
+++ DatingApp/API/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API;
+
+public class AccountController : BaseApiController
+{
+    private DataContext _context;
+    public AccountController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("register")]  // /api/account/register'
+    public async Task<ActionResult<AppUser>> Register(string userName , string password)
+    {
+        using var hmac = new HMACSHA512();
+
+        var user = new AppUser
+        {
+            UserName = userName,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            PassworSalt = hmac.Key
+        };
+
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        return user;
+    }
+
+}
--- DatingApp/API/Controllers/UserController.cs
+++ DatingApp/API/Controllers/UserController.cs
@@ -5,9 +5,7 @@ using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-[ApiController]
-[Route("api/[controller]")]
-public class UsersController : ControllerBase
+public class UsersController : BaseApiController
 {
     private readonly DataContext _context;
     public UsersController(DataContext context)
@@ -22,7 +20,7 @@ public class UsersController : ControllerBase
         return users;
     }
 
-     [HttpGet("{id}")]
+    [HttpGet("{id}")]
     public async Task<ActionResult<AppUser>>GetUser(int id)
     {
         return await _context.Users.FindAsync(id);
